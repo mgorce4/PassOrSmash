d504f5fbff2f768f10233a32afb702f33c7d6d3c
Agahnim <gabrielguysoulard@gmail.com>
2025-04-21 00:01:16 +0200

    Auto stash before rebase of "master" onto "origin/master"
    + add both bags

---
+++ src/main/java/model/people/PeopleBag.java
@@ -0,0 +1,11 @@
+package model.people;
+
+import java.util.Collection;
+
+public class PeopleBag extends InitialBag {
+
+	public PeopleBag(Collection<Person> people) {
+		super(people);
+	}
+
+}
---
+++ src/main/java/model/people/ThingsBag.java
@@ -0,0 +1,11 @@
+package model.people;
+
+import java.util.Collection;
+
+public class ThingsBag extends InitialBag {
+
+	public ThingsBag(Collection<Person> people) {
+		super(people);
+	}
+
+}
