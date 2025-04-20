5b8c790a523bee76ae40c9071aef8a511a641109
Agahnim <gabrielguysoulard@gmail.com>
2025-04-20 23:54:36 +0200

    add Person class

--- src/main/java/model/people/Person.java
+++ src/main/java/model/people/Person.java
@@ -1,5 +1,28 @@
 package model.people;
 
 public class Person {
+	private String name;
+	private String photoPath;
+	
+	public Person(String name, String photoPath) {
+		this.name = name;
+		this.photoPath = photoPath;
+	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhotoPath() {
+		return photoPath;
+	}
+
+	public void setPhotoPath(String photoPath) {
+		this.photoPath = photoPath;
+	}
+	
 }
