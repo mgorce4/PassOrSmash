6da244b7d220b720810f6f4c09133006891f1a47
Agahnim <gabrielguysoulard@gmail.com>
2025-04-21 00:20:12 +0200

    wip people bag

--- src/main/java/model/people/PeopleBag.java
+++ src/main/java/model/people/PeopleBag.java
@@ -1,11 +1,18 @@
 package model.people;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class PeopleBag extends InitialBag {
 
-	public PeopleBag(Collection<Person> people) {
-		super(people);
+	public PeopleBag() {
+		super(new ArrayList<Person>());
+		initializePeopleBag();
+		
+	}
+	
+	public void initializePeopleBag() {
+		this.people.add(new Person("Markiplier", "/images/markiplier.png"));
 	}
 
 }
