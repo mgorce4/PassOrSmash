14b76faf9647d7af840c719b10ecdddb09f16fcf
Agahnim <gabrielguysoulard@gmail.com>
2025-04-21 00:46:32 +0200

    wip michel + main

--- src/main/java/passorsmash/controler/Michel.java
+++ src/main/java/passorsmash/controler/Michel.java
@@ -1,12 +1,34 @@
 package passorsmash.controler;
 
+import java.util.Scanner;
+
+import passorsmash.model.people.PeopleBag;
+import passorsmash.model.people.ThingsBag;
+
 public class Michel implements Michelable {
 	
 	private final String name = "Michel";
 
 	@Override
 	public void initPOS() {
-		System.out.println("Bienvenue dans Pass or Smash !");
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Bienvenue dans Pass or Smash !\nChoisissez un mode !\n1 - Choses\n2 - Personnalités\n");
+		int choixUsr = scanner.nextInt();
+		
+		switch(choixUsr) {
+		case 1 : 
+			ThingsBag thingsbag = new ThingsBag();
+			System.out.println("Vous avez choisi le mode Choses !\nVoici ce que vous allez devoir passer en revue : \n");
+			thingsbag.describe();
+			break;
+		case 2 : 
+			PeopleBag peoplebag = new PeopleBag();
+			System.out.println("Vous avez choisi le mode Personnalités !\nVoici ce que vous allez devoir passer en revue : \n");
+			peoplebag.describe();
+			break;
+		default:
+			System.out.println("Nombre inconnu.");
+		}
 		
 	}
 	
