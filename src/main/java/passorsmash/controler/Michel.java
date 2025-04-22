2f8899313031e02493b9c84c5eb9771252c71b24
toast <159030724+mgorce4@users.noreply.github.com>
2025-04-22 09:46:11 +0200

    make playthrough loop and menu became a method

--- src/main/java/passorsmash/controler/Michel.java
+++ src/main/java/passorsmash/controler/Michel.java
@@ -12,24 +12,33 @@ public class Michel implements Michelable {
 	@Override
 	public void initPOS() {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println(name + ": Bienvenue dans Pass or Smash !\nChoisissez un mode !\n1 - Choses\n2 - Personnalités\n");
-		int choixUsr = scanner.nextInt();
+		int choixUsr = 0;
+		System.out.println(name + ": Bienvenue dans Pass or Smash !");
 		
-		switch(choixUsr) {
-		case 1 : 
-			ThingsBag thingsbag = new ThingsBag();
-			System.out.println("Vous avez choisi le mode Choses !\nVoici ce que vous allez devoir passer en revue : \n");
-			thingsbag.describe();
-			break;
-		case 2 : 
-			PeopleBag peoplebag = new PeopleBag();
-			System.out.println("Vous avez choisi le mode Personnalités !\nVoici ce que vous allez devoir passer en revue : \n");
-			peoplebag.describe();
-			break;
-		default:
-			System.out.println("Nombre inconnu.");
+		while (choixUsr !=1 || choixUsr != 2) {
+			displayMenu();
+			choixUsr = scanner.nextInt();
+			
+			switch(choixUsr) {
+			case 1 : 
+				ThingsBag thingsbag = new ThingsBag();
+				System.out.println("Vous avez choisi le mode Choses !\nVoici ce que vous allez devoir passer en revue : \n");
+				thingsbag.describe();
+				break;
+			case 2 : 
+				PeopleBag peoplebag = new PeopleBag();
+				System.out.println("Vous avez choisi le mode Personnalités !\nVoici ce que vous allez devoir passer en revue : \n");
+				peoplebag.describe();
+				break;
+			default:
+				System.out.println("Nombre inconnu.");
+			}
 		}
 		
+	}
+	
+	public void displayMenu() {
+		System.out.println("Choisissez un mode !\n1 - Choses\n2 - Personnalités\n");
 	}
 	
 	
